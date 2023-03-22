7b0028524f408c1bc6fc2e3e906d1df321c814ab
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-22 23:51:10 +0530

    Delete Everything

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-var PopularTags = ['dragon', 'coffee'];
-// const dragonsTag: MaybePopularTag  = null;
-var dragonsTag = "dragon";
-var username = "alex";
-var pageName = "1";
-var errorMessage = null; //ex- Get Data From DB.. First its null
-var user;
-var someProp;
