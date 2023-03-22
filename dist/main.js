3b90db63900d509d404632fddb3caccb4821b04e
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-22 22:43:59 +0530

    Typescript Type definitions introduction

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,16 @@
-var a = "1";
-console.log("aaa", a);
+var fname = "Dineth";
+var lname = "Siriwardana";
+var c = "d";
+/*
+const hello: "world" - because its const, its type is world
+adter we create a variable We cannot change value to a different type
+let a = "s";
+a = 1 X
+*/
+//
+// function
+// 
+//const getFullName: (fname: String, lname: String) => string
+var getFullName = function (fname, lname) {
+    return fname + " " + lname;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,19 @@
-const a = "1";
-console.log("aaa", a); 
+const fname = "Dineth";  
+let lname = "Siriwardana";
+let c: String = "d";
+ /*
+const hello: "world" - because its const, its type is world
+adter we create a variable We cannot change value to a different type  
+let a = "s";  
+a = 1 X
+*/ 
+
+
+//
+// function
+// 
+
+//const getFullName: (fname: String, lname: String) => string
+const getFullName = (fname : string ,lname : string): string => {
+    return fname + " " + lname;
+}
