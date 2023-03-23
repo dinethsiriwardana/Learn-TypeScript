dd793f393bc67f891da49a9456d7a1dcaa10d366
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-23 08:23:34 +0530

    Working with DOM

---
+++ dist/main.js
@@ -0,0 +1,24 @@
+/*
+
+Here, TS recognizes someElement as a type of element.
+So, the compiler generates an error when we attempt to print the value of sE.:
+        Property 'value' does not exist on type 'Element'.ts(2339)
+
+*/
+var someElement = document.querySelector('.foo'); //(method) ParentNode.querySelector<Element>(selectors: string): Element (+4 overloads)
+//So, We need to convert it to type any
+console.log('someElement', someElement.value);
+//However, that is not the proper method.
+var someElement2 = document.querySelector('.foo');
+console.log('someElement2', someElement2.value);
+/*
+/
+    Listener
+/
+*/
+var listenerElement = document.querySelector('.foo');
+// (parameter) event: Event
+listenerElement.addEventListener('blue', function (event) {
+    // console.log('event',event.target.value) //This is the normal
+    var target = event.target;
+});
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,35 @@
 
 
+/*
+
+Here, TS recognizes someElement as a type of element.
+So, the compiler generates an error when we attempt to print the value of sE.: 
+        Property 'value' does not exist on type 'Element'.ts(2339)
+
+*/
+
+const someElement = document.querySelector('.foo'); //(method) ParentNode.querySelector<Element>(selectors: string): Element (+4 overloads)
+
+//So, We need to convert it to type any
+console.log('someElement',(someElement as any).value) 
+
+//However, that is not the proper method.
+const someElement2 = document.querySelector('.foo') as HTMLInputElement;
+console.log('someElement2',someElement2.value) 
+
+/*
+/
+    Listener
+/
+*/
+const listenerElement = document.querySelector('.foo');
+
+// (parameter) event: Event
+listenerElement.addEventListener('blue',(event) => {
+
+    // console.log('event',event.target.value) //This is the normal
+    
+
+    const target = event.target as HTMLInputElement
+});
+
