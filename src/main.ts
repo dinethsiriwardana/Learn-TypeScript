1221bc09d21730831a07adfda5af55b9ee45dc5b
Dineth Siriwardana <dinethsiriwardana28@gmail.com>
2023-03-23 08:25:05 +0530

    Delete All

--- src/main.ts
+++ src/main.ts
@@ -10,26 +10,3 @@ So, the compiler generates an error when we attempt to print the value of sE.:
 
 const someElement = document.querySelector('.foo'); //(method) ParentNode.querySelector<Element>(selectors: string): Element (+4 overloads)
 
-//So, We need to convert it to type any
-console.log('someElement',(someElement as any).value) 
-
-//However, that is not the proper method.
-const someElement2 = document.querySelector('.foo') as HTMLInputElement;
-console.log('someElement2',someElement2.value) 
-
-/*
-/
-    Listener
-/
-*/
-const listenerElement = document.querySelector('.foo');
-
-// (parameter) event: Event
-listenerElement.addEventListener('blue',(event) => {
-
-    // console.log('event',event.target.value) //This is the normal
-    
-
-    const target = event.target as HTMLInputElement
-});
-
